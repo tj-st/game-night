587e32c5a05343e8b4ed6da5a99916f12713f5e9
ddot <todd@ddot.gg>
2022-07-20 17:03:19 -0400

    chore: css text color

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,7 +1,7 @@
 const PageHeader = () => {
   return (
     <div className="h-20 bg-orange-400 pl-5 flex items-center">
-      <h1 className="font-bold text-5xl drop-shadow-md">GN | EVENTS</h1>
+      <h1 className="font-bold text-5xl drop-shadow-md text-[#222222]">GN | EVENTS</h1>
     </div>
   );
 };
